273829748c27098b6db7af7b100e25984825a8f5
Perry Kwarteng <perrykwarteng8@gmail.com>
2023-07-08 04:06:53 +0100

    added controller,model and migration for classes, subjects,students, and teachers

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teaher;
+use Illuminate\Http\Request;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Teaher  $teaher
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teaher $teaher)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Teaher  $teaher
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Teaher $teaher)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Teaher  $teaher
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Teaher $teaher)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Teaher  $teaher
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teaher $teaher)
+    {
+        //
+    }
+}
--- app/Models/superadmin.php
+++ app/Models/superadmin.php
@@ -5,12 +5,13 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-
-class superadmin extends Model
+class superadmin extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $fillable = [
         'adminIndex',
